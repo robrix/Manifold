93ad6311ce57ed86b5dc960deaa231c7d8506a10
Rob Rix <rob.rix@me.com>
2018-05-20 10:11:12 -0400

    Define a typing relation.

--- src/Manifold/Judgement.hs
+++ src/Manifold/Judgement.hs
@@ -1,5 +1,6 @@
 module Manifold.Judgement where
 
+import Data.Semiring (zero)
 import Manifold.Context
 import Manifold.Presyntax
 
@@ -16,3 +17,14 @@ data Judgement usage
 data Assumptions usage
   = Zeroed  (Context usage)
   | Assumed (Context usage)
+
+
+typing :: Monoid usage => Judgement usage -> Either (Judgement usage) [Judgement usage]
+typing judgement = case judgement of
+  Zeroed gamma :- [ Check (Type Bool) ] ->
+    Right [ Zeroed gamma |- [] ]
+  Zeroed gamma :- [ Check (Type (((x, _) ::: _S) :-> _T)) ] ->
+    Right [ Zeroed gamma |- [ Check _S ]
+          , Zeroed (gamma :> ((x, zero) ::: _S)) |- [ Check _T ]
+          ]
+  other -> Left other
