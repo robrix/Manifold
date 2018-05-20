55929a70844d2535f8420baf9f5eba7c669c97a4
Rob Rix <rob.rix@me.com>
2018-05-20 10:10:50 -0400

    Add a fancier constructor for assumptions.

--- src/Manifold/Judgement.hs
+++ src/Manifold/Judgement.hs
@@ -10,6 +10,9 @@ data Proposition usage
 data Judgement usage
   = Assumptions usage :- [Proposition usage]
 
+(|-) :: Assumptions usage -> [Proposition usage] -> Judgement usage
+(|-) = (:-)
+
 data Assumptions usage
   = Zeroed  (Context usage)
   | Assumed (Context usage)
