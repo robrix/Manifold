a6392f26b82eec845be7595a1b5dcaf4f250b9b0
Rob Rix <rob.rix@me.com>
2018-05-20 14:07:48 -0400

    Define a helper to lookup in a substitution.

--- src/Manifold/Substitution.hs
+++ src/Manifold/Substitution.hs
@@ -7,6 +7,10 @@ import Manifold.Name
 newtype Substitution term = Substitution { getSubstitution :: Map.Map Name term }
   deriving (Eq, Foldable, Ord, Show)
 
+
+lookupSubst :: Name -> Substitution term -> Maybe term
+lookupSubst name = Map.lookup name . getSubstitution
+
 deleteSubst :: Name -> Substitution term -> Substitution term
 deleteSubst name = Substitution . Map.delete name . getSubstitution
 
