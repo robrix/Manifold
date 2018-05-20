6365d34d007246cf5bce244cd49b7f40bb1e6ed2
Rob Rix <rob.rix@me.com>
2018-05-19 20:41:59 -0400

    Define Semigroup, Monoid, Semiring, & Unital instances for Erasure.

--- src/Manifold/Erasure.hs
+++ src/Manifold/Erasure.hs
@@ -1,6 +1,13 @@
 module Manifold.Erasure where
 
+import Data.Semiring (Semiring(..), Unital(..))
+
 data Erasure
   = Erased
   | Present
   deriving (Eq, Ord, Show)
+
+instance Semigroup Erasure where (<>)   = max
+instance Monoid    Erasure where mempty = Erased
+instance Semiring  Erasure where (><)   = min
+instance Unital    Erasure where one    = Present
