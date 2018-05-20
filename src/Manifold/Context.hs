452fa2ad6f0d2dd012d35a819f5727615d9c7ce4
Rob Rix <rob.rix@me.com>
2018-05-19 21:28:06 -0400

    Define a Semigroup instance for Contexts.

--- src/Manifold/Context.hs
+++ src/Manifold/Context.hs
@@ -8,3 +8,12 @@ data Context usage
   deriving (Eq, Ord, Show)
 
 infixl 5 :>
+
+
+instance (Eq usage, Semigroup usage) => Semigroup (Context usage) where
+  Empty <> Empty = Empty
+  (ctx1 :> name1 :@ u1 ::: t1) <> (ctx2 :> name2 :@ u2 ::: t2)
+    | name1 == name2
+    , t1 == t2
+    = (ctx1 <> ctx2) :> name1 :@ (u1 <> u2) ::: t1
+  _ <> _ = error "adding inequal contexts"
