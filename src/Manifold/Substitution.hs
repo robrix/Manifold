71ec0d7467db1402cb7c1545529f1d4540e5ba84
Rob Rix <rob.rix@me.com>
2018-05-20 13:31:37 -0400

    Define composition of substitutions.

--- src/Manifold/Substitution.hs
+++ src/Manifold/Substitution.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE DeriveFoldable, GeneralizedNewtypeDeriving #-}
 module Manifold.Substitution where
 
+import Data.Maybe (isNothing)
 import Manifold.Name
 
 newtype Substitution term = Substitution { getSubstitution :: [(Name, term)] }
   deriving (Eq, Foldable, Ord, Show)
+
+instance Semigroup (Substitution term) where
+  Substitution as <> Substitution bs = Substitution (as <> filter (isNothing . flip lookup as . fst) bs)
