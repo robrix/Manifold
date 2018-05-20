9ffb3773a398e53719b50ff4c360de559ad363b9
Rob Rix <rob.rix@me.com>
2018-05-20 14:08:43 -0400

    Unification builds and applies substitutions.

--- src/Manifold/Unification.hs
+++ src/Manifold/Unification.hs
@@ -2,8 +2,10 @@
 module Manifold.Unification where
 
 import Control.Monad.Effect
+import Control.Monad.Effect.State
 import Manifold.Presyntax
 import Manifold.Proof
+import Manifold.Substitution
 
 unify :: Member (Unify usage) effects => Type usage -> Type usage -> Proof usage effects (Type usage)
 unify actual expected = send (Unify actual expected)
@@ -22,9 +24,9 @@ unification (Unify actual expected)
 runUnification :: ( Eq usage
                   , Member (Exc (Some (Unify usage))) effects
                   )
-               => Proof usage (Unify usage ': effects) a
-               -> Proof usage effects a
-runUnification = refine unification
+               => Proof usage (Unify usage ': State (Substitution (Type usage)) ': effects) (Type usage)
+               -> Proof usage effects (Type usage)
+runUnification = fmap (uncurry (flip apply)) . runState mempty . refine unification
 
 
 data Unify usage result where
