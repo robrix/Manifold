f37392b2feb7a4bd0934b285f8e225670e9a6c65
Rob Rix <rob.rix@me.com>
2018-05-20 16:36:25 -0400

    Product types are types.

--- src/Manifold/Judgement.hs
+++ src/Manifold/Judgement.hs
@@ -29,6 +29,7 @@ typeFormation (CheckIsType tm) = Type <$> case unTerm tm of
     _S' <- checkIsType _S
     _T' <- (name, zero) ::: _S' >- checkIsType _T
     pure ((name, usage) ::: _S' :-> _T')
+  _S :* _T -> (:*) <$> checkIsType _S <*> checkIsType _T
   _ -> noRuleTo (CheckIsType tm)
 
 
