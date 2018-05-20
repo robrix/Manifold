57f842a23aea6c635b7f360e609aee6d395a55f6
Rob Rix <rob.rix@me.com>
2018-05-19 22:57:34 -0400

    Define a Judgement datatype.

--- src/Manifold/Judgement.hs
+++ src/Manifold/Judgement.hs
@@ -1,6 +1,10 @@
 module Manifold.Judgement where
 
+import Manifold.Context
 import Manifold.Presyntax
 
 data Proposition usage
   = Type usage :=: Type usage
+
+data Judgement usage
+  = Context usage :- [Proposition usage]
