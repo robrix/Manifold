e42597e5622be3dbc48fefd15231b4ce2e391f4a
Rob Rix <rob.rix@me.com>
2018-05-19 23:36:26 -0400

    Define a Unital instance for Purpose.

--- src/Manifold/Purpose.hs
+++ src/Manifold/Purpose.hs
@@ -1,6 +1,6 @@
 module Manifold.Purpose where
 
-import Data.Semiring (Semiring(..))
+import Data.Semiring (Semiring(..), Unital(..))
 
 data Purpose
   = Extensional
@@ -9,4 +9,5 @@ data Purpose
 
 instance Semigroup Purpose where (<>)   = max
 instance Monoid    Purpose where mempty = Extensional
-instance Semiring  Purpose where (><) = min
+instance Semiring  Purpose where (><)   = min
+instance Unital    Purpose where one    = Intensional
