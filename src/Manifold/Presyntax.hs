e2f07518e328adbd206f20d363ba1bc835bde620
Rob Rix <rob.rix@me.com>
2018-05-19 22:36:37 -0400

    Define a Bifoldable instance for Expr & Foldable instances for Constraint & Type.

--- src/Manifold/Presyntax.hs
+++ src/Manifold/Presyntax.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable #-}
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, LambdaCase #-}
 module Manifold.Presyntax where
 
+import Data.Bifoldable
 import Manifold.Name
 
 data Constraint usage = Binding usage ::: Type usage
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Foldable, Ord, Show)
 
 infix 5 :::
 
@@ -23,8 +24,24 @@ data Expr usage recur
   | If recur recur recur
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
+instance Bifoldable Expr where
+  bifoldMap f g = \case
+    Bool         -> mempty
+    T            -> mempty
+    F            -> mempty
+    Set          -> mempty
+    var :-> body -> foldMap f var <> g body
+    Var _        -> mempty
+    Abs var body -> foldMap f var <> g body
+    App f a      -> g f <> g a
+    If c t e     -> g c <> g t <> g e
+
+
 newtype Type usage = Type { unType :: Expr usage (Type usage) }
   deriving (Eq, Ord, Show)
+
+instance Foldable Type where
+  foldMap f = bifoldMap f (foldMap f) . unType
 
 newtype Term usage = Term { unTerm :: Expr usage (Term usage) }
   deriving (Eq, Ord, Show)
