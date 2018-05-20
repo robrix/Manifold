8cfcc49368fa7837fdd0517d9af54a744cba66d8
Rob Rix <rob.rix@me.com>
2018-05-20 13:30:59 -0400

    Derive some instances.

--- src/Manifold/Substitution.hs
+++ src/Manifold/Substitution.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE DeriveFoldable, GeneralizedNewtypeDeriving #-}
 module Manifold.Substitution where
 
 import Manifold.Name
 
 newtype Substitution term = Substitution { getSubstitution :: [(Name, term)] }
+  deriving (Eq, Foldable, Ord, Show)
