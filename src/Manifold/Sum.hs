89de00687c19c28b7db53096cee48061d16d65d9
Rob Rix <rob.rix@me.com>
2018-05-21 11:51:08 -0400

    Decompose sums.

--- src/Manifold/Sum.hs
+++ src/Manifold/Sum.hs
@@ -2,6 +2,7 @@
 module Manifold.Sum
 ( Sum
 , Element(..)
+, decompose
 ) where
 
 data Sum ts a where
@@ -22,3 +23,8 @@ instance {-# OVERLAPPABLE #-} Element t ts => Element t (t' ': ts) where
   inject = There . inject
   project (There t) = project t
   project _         = Nothing
+
+
+decompose :: Sum (t ': ts) a -> Either (t a) (Sum ts a)
+decompose (Here t) = Left t
+decompose (There ts) = Right ts
