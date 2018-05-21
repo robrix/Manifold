a4a2b54c84d1accbe4e69ba29552aa3e8118af3b
Rob Rix <rob.rix@me.com>
2018-05-21 17:46:02 -0400

    Derive Foldable, Functor, & Traversable instances for Binding.

--- src/Manifold/Binding.hs
+++ src/Manifold/Binding.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable #-}
 module Manifold.Binding where
 
 import Manifold.Name
 
 data Binding usage = Binding { bindingName :: Name, bindingUsage :: usage }
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 instance Named (Binding usage) where
   name = name . bindingName
