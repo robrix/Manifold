ba7e04e0d7e1d217fcd5e0ad07b56e44fc1115d6
Rob Rix <rob.rix@me.com>
2018-05-21 16:34:47 -0400

    Stub in an elaborated term type.

--- src/Manifold/Elaborate.hs
+++ src/Manifold/Elaborate.hs
@@ -1,1 +1,7 @@
 module Manifold.Elaborate where
+
+import Manifold.Constraint
+import Manifold.Expr
+
+newtype Elab usage = Elab { unElab :: Expr (Constraint usage (Elab usage)) (Elab usage) }
+  deriving (Eq, Ord)
