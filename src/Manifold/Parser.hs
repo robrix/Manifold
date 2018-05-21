b308c9e0b0e8114583e1f13e8d0a186f27acbdb0
Rob Rix <rob.rix@me.com>
2018-05-20 20:36:30 -0400

    Define a Parser type.

--- src/Manifold/Parser.hs
+++ src/Manifold/Parser.hs
@@ -1,1 +1,16 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Manifold.Parser where
+
+import Control.Applicative (Alternative(..))
+import Text.Parser.Char
+import Text.Parser.Combinators
+import Text.Parser.Token
+import Text.Parser.Token.Style
+
+newtype Parser f a = Parser { runParser :: f a }
+  deriving (Alternative, Applicative, CharParsing, Functor, Monad, Parsing)
+
+instance TokenParsing f => TokenParsing (Parser f) where
+  someSpace = Parser $ buildSomeSpaceParser someSpace haskellCommentStyle
+  nesting = Parser . nesting . runParser
+  highlight h = Parser . highlight h . runParser
