7fd019b58316e2bb3c40c5629aa4f9c4d0498dbf
Rob Rix <rob.rix@me.com>
2018-05-21 11:45:37 -0400

    Define a Union datatype.

--- src/Manifold/Union.hs
+++ src/Manifold/Union.hs
@@ -1,1 +1,6 @@
+{-# LANGUAGE DataKinds, GADTs, TypeOperators #-}
 module Manifold.Union where
+
+data Union ts a where
+  Here :: t a -> Union (t ': ts) a
+  There :: Union ts a -> Union (t ': ts) a
