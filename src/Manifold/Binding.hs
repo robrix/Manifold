cc23636d54c46333c516e4ddd0f7d76653e4f5b2
Rob Rix <rob.rix@me.com>
2018-05-21 17:43:43 -0400

    Binding is Named.

--- src/Manifold/Binding.hs
+++ src/Manifold/Binding.hs
@@ -4,3 +4,7 @@ import Manifold.Name
 
 data Binding usage = Binding { bindingName :: Name, bindingUsage :: usage }
   deriving (Eq, Ord, Show)
+
+instance Named (Binding usage) where
+  name = name . bindingName
+  setName n b = b { bindingName = n }
