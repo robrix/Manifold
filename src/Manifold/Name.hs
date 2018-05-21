ab1ab9f7e34cd69e06a55ebd4237b49e162088d2
Rob Rix <rob.rix@me.com>
2018-05-21 17:33:22 -0400

    Named includes setName.

--- src/Manifold/Name.hs
+++ src/Manifold/Name.hs
@@ -8,9 +8,12 @@ data Name
 
 class Named n where
   name :: n -> Name
+  setName :: Name -> n -> n
 
 instance Named Name where
   name = id
+  setName = const
 
 instance Named a => Named (a, b) where
   name = name . fst
+  setName n = (,) . setName n . fst <*> snd
