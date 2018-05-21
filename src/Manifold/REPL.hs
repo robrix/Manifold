31e4722dc4a88b03b465cb144de54642dc642c4b
Rob Rix <rob.rix@me.com>
2018-05-20 20:25:38 -0400

    Add some machinery to run prompts.

--- src/Manifold/REPL.hs
+++ src/Manifold/REPL.hs
@@ -1,12 +1,37 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, GADTs, LambdaCase, TypeOperators #-}
 module Manifold.REPL where
 
+import Control.Monad.Effect
 import Manifold.Expr
+import System.Console.Haskeline
 
 data REPL usage result where
   TypeOf :: Term usage -> REPL usage (Type usage)
   Quit :: REPL usage ()
 
 data Prompt result where
-  Prompt :: Prompt String
+  Prompt :: Prompt (Maybe String)
   Output :: String -> Prompt ()
+
+runPrompt :: String -> Eff '[Prompt] a -> IO a
+runPrompt prompt action = do
+  prefs <- readPrefs "~/.local/Manifold/repl_prefs"
+  runInputTWithPrefs prefs settings (runM (reinterpret (\case
+    Prompt -> sendInputT (getInputLine (cyan <> prompt <> plain))
+    Output s -> sendInputT (outputStrLn s)) action))
+
+cyan :: String
+cyan = "\ESC[1;36m\STX"
+
+plain :: String
+plain = "\ESC[0m\STX"
+
+sendInputT :: Member (InputT IO) effects => InputT IO a -> Eff effects a
+sendInputT = send
+
+settings :: Settings IO
+settings = Settings
+  { complete = noCompletion
+  , historyFile = Just "~/.local/Manifold/repl_history"
+  , autoAddHistory = True
+  }
