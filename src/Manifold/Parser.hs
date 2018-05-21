f082defbfd0ffa4937ae45c52029802622606f7f
Rob Rix <rob.rix@me.com>
2018-05-20 20:38:47 -0400

    Add conveniences to parse a string.

--- src/Manifold/Parser.hs
+++ src/Manifold/Parser.hs
@@ -6,6 +6,7 @@ import Text.Parser.Char
 import Text.Parser.Combinators
 import Text.Parser.Token
 import Text.Parser.Token.Style
+import qualified Text.Trifecta as Trifecta
 
 newtype Parser f a = Parser { runParser :: f a }
   deriving (Alternative, Applicative, CharParsing, Functor, Monad, Parsing)
@@ -14,3 +15,11 @@ instance TokenParsing f => TokenParsing (Parser f) where
   someSpace = Parser $ buildSomeSpaceParser someSpace haskellCommentStyle
   nesting = Parser . nesting . runParser
   highlight h = Parser . highlight h . runParser
+
+parseString :: Parser Trifecta.Parser a -> String -> Either [String] a
+parseString (Parser p) = toResult . Trifecta.parseString p mempty
+
+toResult :: Trifecta.Result a -> Either [String] a
+toResult r = case r of
+  Trifecta.Success a -> Right a
+  Trifecta.Failure info -> Left [show (Trifecta._errDoc info)]
