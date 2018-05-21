91955e83dfa220576a65ca48b5bd998e8a2f2cce
Rob Rix <rob.rix@me.com>
2018-05-20 20:04:31 -0400

    Define a Prompt effect.

--- src/Manifold/REPL.hs
+++ src/Manifold/REPL.hs
@@ -6,3 +6,7 @@ import Manifold.Expr
 data REPL usage result where
   TypeOf :: Term usage -> REPL usage (Type usage)
   Quit :: REPL usage ()
+
+data Prompt result where
+  Prompt :: Prompt String
+  Output :: String -> Prompt ()
