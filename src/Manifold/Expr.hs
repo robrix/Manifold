767ed80143d2e23fe882e6c33c1a22d95943d487
Rob Rix <rob.rix@me.com>
2018-05-21 16:40:57 -0400

    Move Intro into its own module.

--- src/Manifold/Expr.hs
+++ src/Manifold/Expr.hs
@@ -17,52 +17,8 @@ import Data.Functor.Foldable (Base, Corecursive(..), Recursive(..))
 import qualified Data.Set as Set
 import Manifold.Constraint
 import Manifold.Expr.Elim
+import Manifold.Expr.Intro
 import Manifold.Name
-
-data Intro var scope recur
-  = Unit
-  | Bool Bool
-  | Abs var scope
-  | Pair recur recur
-  | UnitT
-  | BoolT
-  | TypeT
-  | var :-> scope
-  | recur :* recur
-  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
-
-infixr 0 :->
-infixl 7 :*
-
-class Trifoldable t where
-  trifoldMap :: Monoid m => (a -> m) -> (b -> m) -> (c -> m) -> t a b c -> m
-
-class Trifunctor t where
-  trimap :: (a -> a') -> (b -> b') -> (c -> c') -> t a b c -> t a' b' c'
-
-instance Trifoldable Intro where
-  trifoldMap f g h = \case
-    Unit     -> mempty
-    Bool _   -> mempty
-    Abs v b  -> f v <> g b
-    Pair a b -> h a <> h b
-    UnitT    -> mempty
-    BoolT    -> mempty
-    TypeT    -> mempty
-    v :-> b  -> f v <> g b
-    a :* b   -> h a <> h b
-
-instance Trifunctor Intro where
-  trimap f g h = \case
-    Unit     -> Unit
-    Bool b   -> Bool b
-    Abs v b  -> f v `Abs` g b
-    Pair a b -> h a `Pair` h b
-    UnitT    -> UnitT
-    BoolT    -> BoolT
-    TypeT    -> TypeT
-    v :-> b  -> f v :-> g b
-    a :* b   -> h a :* h b
 
 data Expr var recur
   = Var Name
--- src/Manifold/Expr/Intro.hs
+++ src/Manifold/Expr/Intro.hs
@@ -1,1 +1,47 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, LambdaCase #-}
 module Manifold.Expr.Intro where
+
+data Intro var scope recur
+  = Unit
+  | Bool Bool
+  | Abs var scope
+  | Pair recur recur
+  | UnitT
+  | BoolT
+  | TypeT
+  | var :-> scope
+  | recur :* recur
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+infixr 0 :->
+infixl 7 :*
+
+class Trifoldable t where
+  trifoldMap :: Monoid m => (a -> m) -> (b -> m) -> (c -> m) -> t a b c -> m
+
+class Trifunctor t where
+  trimap :: (a -> a') -> (b -> b') -> (c -> c') -> t a b c -> t a' b' c'
+
+instance Trifoldable Intro where
+  trifoldMap f g h = \case
+    Unit     -> mempty
+    Bool _   -> mempty
+    Abs v b  -> f v <> g b
+    Pair a b -> h a <> h b
+    UnitT    -> mempty
+    BoolT    -> mempty
+    TypeT    -> mempty
+    v :-> b  -> f v <> g b
+    a :* b   -> h a <> h b
+
+instance Trifunctor Intro where
+  trimap f g h = \case
+    Unit     -> Unit
+    Bool b   -> Bool b
+    Abs v b  -> f v `Abs` g b
+    Pair a b -> h a `Pair` h b
+    UnitT    -> UnitT
+    BoolT    -> BoolT
+    TypeT    -> TypeT
+    v :-> b  -> f v :-> g b
+    a :* b   -> h a :* h b
