44116267e5612fabbfa5d3d58217c4354972ff6a
Rob Rix <rob.rix@me.com>
2018-05-21 09:27:32 -0400

    Define a Value datatype.

--- src/Manifold/Value.hs
+++ src/Manifold/Value.hs
@@ -1,1 +1,15 @@
 module Manifold.Value where
+
+import Manifold.Context
+import Manifold.Expr hiding (Unit, Pair)
+import Manifold.Name
+
+data Value usage
+  = Closure Name (Term usage) (Context usage (Value usage))
+  | Unit
+  | UnitT
+  | Bool Bool
+  | BoolT
+  | TypeT
+  | Pair (Value usage) (Value usage)
+  | Product (Value usage) (Value usage)
