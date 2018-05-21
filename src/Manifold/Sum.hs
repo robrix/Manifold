aff0f7c281d0f1f8ea5fef71f378b99acc944cd0
Rob Rix <rob.rix@me.com>
2018-05-21 11:50:14 -0400

    Spacing.

--- src/Manifold/Sum.hs
+++ src/Manifold/Sum.hs
@@ -8,6 +8,7 @@ data Sum ts a where
   Here :: t a -> Sum (t ': ts) a
   There :: Sum ts a -> Sum (t ': ts) a
 
+
 class Element t ts where
   inject :: t a -> Sum ts a
   project :: Sum ts a -> Maybe (t a)
