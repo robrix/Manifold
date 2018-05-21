c2a6e3359d49cf84a1ab5a94868ef79fc1457fe3
Rob Rix <rob.rix@me.com>
2018-05-21 12:03:47 -0400

    Define a bunch of empty instances for empty sums.

--- src/Manifold/Sum.hs
+++ src/Manifold/Sum.hs
@@ -42,22 +42,40 @@ weaken = There
 instance (Foldable t, Foldable (Sum ts)) => Foldable (Sum (t ': ts)) where
   foldMap f = either (foldMap f) (foldMap f) . decompose
 
+instance Foldable (Sum '[]) where
+  foldMap _ u = case u of {}
+
 instance (Functor t, Functor (Sum ts)) => Functor (Sum (t ': ts)) where
   fmap f = either (inject . fmap f) (weaken . fmap f) . decompose
+
+instance Functor (Sum '[]) where
+  fmap _ u = case u of {}
 
 instance (Traversable t, Traversable (Sum ts)) => Traversable (Sum (t ': ts)) where
   traverse f = either (fmap inject . traverse f) (fmap weaken . traverse f) . decompose
 
+instance Traversable (Sum '[]) where
+  traverse _ u = case u of {}
+
 instance (Show (t a), Show (Sum ts a)) => Show (Sum (t ': ts) a) where
   showsPrec d = either (showsPrec d) (showsPrec d) . decompose
+
+instance Show (Sum '[] a) where
+  showsPrec _ u = case u of {}
 
 instance (Eq (t a), Eq (Sum ts a)) => Eq (Sum (t ': ts) a) where
   Here t1 == Here t2 = t1 == t2
   There ts1 == There ts2 = ts1 == ts2
   _ == _ = False
 
+instance Eq (Sum '[] a) where
+  u1 == _ = case u1 of {}
+
 instance (Ord (t a), Ord (Sum ts a)) => Ord (Sum (t ': ts) a) where
   compare (Here t1) (Here t2) = compare t1 t2
   compare (There ts1) (There ts2) = compare ts1 ts2
   compare (Here _) (There _) = LT
   compare _ _ = GT
+
+instance Ord (Sum '[] a) where
+  compare u1 = case u1 of {}
