4cc12e564cde80edac274bf9b7a9610792f57485
Rob Rix <rob.rix@me.com>
2018-05-21 18:15:06 -0400

    Define Context as a newtype wrapping an ordinary list.

--- src/Manifold/Context.hs
+++ src/Manifold/Context.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
 module Manifold.Context
 ( Context
 , contextLookup
@@ -7,54 +7,40 @@ module Manifold.Context
 , (|>)
 ) where
 
+import Data.Bifunctor
+import Data.List
 import Data.Module.Class
 import Data.Semiring (Semiring(..))
 import Manifold.Binding
 import Manifold.Constraint
 import Manifold.Name
 
-data Context usage recur
-  = Empty
-  | Context usage recur :> Constraint usage recur
+newtype Context usage recur = Context { unContext :: [Constraint usage recur] }
   deriving (Eq, Ord, Show)
 
-infixl 5 :>
-
-
 contextLookup :: Name -> Context usage recur -> Maybe (Constraint usage recur)
-contextLookup name = go
-  where go (context :> constraint)
-          | constraintName constraint == name = Just constraint
-          | otherwise                         = contextLookup name context
-        go Empty                              = Nothing
+contextLookup name = find ((== name) . constraintName) . unContext
 
 
 contextFilter :: (Constraint usage recur -> Bool) -> Context usage recur -> Context usage recur
-contextFilter keep = go
-  where go (context :> constraint)
-          | keep constraint = go context :> constraint
-          | otherwise       = go context
-        go Empty            = Empty
+contextFilter keep = Context . filter keep . unContext
 
 
 instance (Eq recur, Eq usage, Semigroup usage) => Semigroup (Context usage recur) where
-  Empty <> Empty = Empty
-  (ctx1 :> (Binding name1 u1 ::: t1)) <> (ctx2 :> (Binding name2 u2 ::: t2))
-    | name1 == name2
-    , t1 == t2
-    = (ctx1 <> ctx2) :> (Binding name1 (u1 <> u2) ::: t1)
-  _ <> _ = error "adding inequal contexts"
+  Context as <> Context bs = Context (zipWith go as bs)
+    where go (Binding name1 u1 ::: t1) (Binding name2 u2 ::: t2)
+            | name1 == name2, t1 == t2 = Binding name1 (u1 <> u2) ::: t1
+            | otherwise                = error "adding inequal contexts"
 
 
 instance (Eq recur, Eq usage, Semiring usage) => Module usage (Context usage recur) where
-  _  ><< Empty = Empty
-  u1 ><< (ctx :> (Binding name u2 ::: t)) = (u1 ><< ctx) :> (Binding name (u1 >< u2) ::: t)
+  u ><< Context as = Context (map (first (u ><)) as)
 
 
 (|>) :: Context usage recur -> Constraint usage recur -> Context usage recur
-(|>) = (:>)
+(|>) = fmap Context . flip (:) . unContext
 
 infixl 5 |>
 
 emptyContext :: Context usage recur
-emptyContext = Empty
+emptyContext = Context []
