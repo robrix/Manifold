829248f6728633526f03a923945394f8f3efab73
Rob Rix <rob.rix@me.com>
2018-05-21 09:16:40 -0400

    Define a function to filter a context.

--- src/Manifold/Context.hs
+++ src/Manifold/Context.hs
@@ -21,6 +21,14 @@ contextFind name (context :> constraint)
 contextFind _    Empty                = Nothing
 
 
+contextFilter :: (Constraint usage recur -> Bool) -> Context usage recur -> Context usage recur
+contextFilter keep = go
+  where go (context :> constraint)
+          | keep constraint = go context :> constraint
+          | otherwise       = go context
+        go Empty            = Empty
+
+
 instance (Eq recur, Eq usage, Semigroup usage) => Semigroup (Context usage recur) where
   Empty <> Empty = Empty
   (ctx1 :> ((name1, u1) ::: t1)) <> (ctx2 :> ((name2, u2) ::: t2))
