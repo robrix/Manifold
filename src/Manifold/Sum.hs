61a3136d06dcac898845f8ced5bb6dd1ec741899
Rob Rix <rob.rix@me.com>
2018-05-21 11:47:25 -0400

    Define an Element class.

--- src/Manifold/Sum.hs
+++ src/Manifold/Sum.hs
@@ -1,8 +1,13 @@
-{-# LANGUAGE DataKinds, GADTs, TypeOperators #-}
+{-# LANGUAGE DataKinds, GADTs, MultiParamTypeClasses, TypeOperators #-}
 module Manifold.Sum
 ( Sum
+, Element(..)
 ) where
 
 data Sum ts a where
   Here :: t a -> Sum (t ': ts) a
   There :: Sum ts a -> Sum (t ': ts) a
+
+class Element t ts where
+  inject :: t a -> Sum ts a
+  project :: Sum ts a -> Maybe (t a)
