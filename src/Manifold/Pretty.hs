18282111adc39b43179ba2366d75a70f8a4fa444
Rob Rix <rob.rix@me.com>
2018-05-22 21:24:19 -0400

    Define a prettys helper.

--- src/Manifold/Pretty.hs
+++ src/Manifold/Pretty.hs
@@ -3,5 +3,8 @@ module Manifold.Pretty where
 class Pretty a where
   prettyPrec :: Int -> a -> ShowS
 
+prettys :: Pretty a => a -> ShowS
+prettys = prettyPrec 0
+
 pretty :: Pretty a => a -> String
-pretty = ($ "") . prettyPrec 0
+pretty = ($ "") . prettys
