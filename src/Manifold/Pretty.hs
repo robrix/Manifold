4feda7ba0ddc6ee8a52b79ba7c83198b3920bf81
Rob Rix <rob.rix@me.com>
2018-05-22 22:20:04 -0400

    Give a default signature for Pretty in terms of Show.

--- src/Manifold/Pretty.hs
+++ src/Manifold/Pretty.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE DefaultSignatures #-}
 module Manifold.Pretty where
 
 class Pretty a where
   prettyPrec :: Int -> a -> ShowS
+  default prettyPrec :: Show a => Int -> a -> ShowS
+  prettyPrec = showsPrec
 
 prettys :: Pretty a => a -> ShowS
 prettys = prettyPrec 0
