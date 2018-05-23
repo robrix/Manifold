cebdf3941f9b6265df1e83e59f7d93e7c4861b3c
Rob Rix <rob.rix@me.com>
2018-05-22 22:25:03 -0400

    Define a Pretty instance for Binding.

--- src/Manifold/Binding.hs
+++ src/Manifold/Binding.hs
@@ -2,6 +2,7 @@
 module Manifold.Binding where
 
 import Manifold.Name
+import Manifold.Pretty
 
 data Binding usage = Binding { bindingName :: Name, bindingUsage :: usage }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
@@ -9,3 +10,6 @@ data Binding usage = Binding { bindingName :: Name, bindingUsage :: usage }
 instance Named (Binding usage) where
   name = name . bindingName
   setName n b = b { bindingName = n }
+
+instance Pretty usage => Pretty (Binding usage) where
+  prettyPrec d (Binding name usage) = prettyPrec d name . showChar ' ' . showChar '@' . prettyPrec d usage
