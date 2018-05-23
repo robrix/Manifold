0fbed36fcc899560151b837c58f1a1056a011f28
Rob Rix <rob.rix@me.com>
2018-05-22 21:34:01 -0400

    Define a Pretty instance for Elim.

--- src/Manifold/Expr/Elim.hs
+++ src/Manifold/Expr/Elim.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable #-}
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, LambdaCase #-}
 module Manifold.Expr.Elim where
+
+import Data.Functor.Classes
+import Manifold.Pretty
 
 data Elim recur
   = ExL recur
@@ -7,3 +10,13 @@ data Elim recur
   | App recur recur
   | If recur recur recur
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Pretty recur => Pretty (Elim recur) where
+  prettyPrec d = \case
+    ExL a -> showsUnaryWith prettyPrec "exl" d a
+    ExR a -> showsUnaryWith prettyPrec "exr" d a
+    App f a -> showParen (d > 10) $ prettyPrec 10 f . showChar ' ' . prettyPrec 11 a
+    If c t e -> showParen (d > (-1))
+      $ showString "if"   . showChar ' ' . prettyPrec 0    c . showChar ' '
+      . showString "then" . showChar ' ' . prettyPrec 0    t . showChar ' '
+      . showString "then" . showChar ' ' . prettyPrec (-1) e
