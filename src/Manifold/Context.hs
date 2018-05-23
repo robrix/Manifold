18f731fd7bdf8c5d0502f3779dd85fe138f4f4c6
Rob Rix <rob.rix@me.com>
2018-05-22 21:55:37 -0400

    Rename Context’s second parameter to ty.

--- src/Manifold/Context.hs
+++ src/Manifold/Context.hs
@@ -15,32 +15,32 @@ import Manifold.Binding
 import Manifold.Constraint
 import Manifold.Name
 
-newtype Context var recur = Context { unContext :: [Constraint var recur] }
+newtype Context var ty = Context { unContext :: [Constraint var ty] }
   deriving (Eq, Ord, Show)
 
-contextLookup :: Named var => Name -> Context var recur -> Maybe (Constraint var recur)
+contextLookup :: Named var => Name -> Context var ty -> Maybe (Constraint var ty)
 contextLookup name = find ((== name) . constraintName) . unContext
 
 
-contextFilter :: (Constraint var recur -> Bool) -> Context var recur -> Context var recur
+contextFilter :: (Constraint var ty -> Bool) -> Context var ty -> Context var ty
 contextFilter keep = Context . filter keep . unContext
 
 
-instance (Eq recur, Eq usage, Semigroup usage) => Semigroup (Context (Binding usage) recur) where
+instance (Eq ty, Eq usage, Semigroup usage) => Semigroup (Context (Binding usage) ty) where
   Context as <> Context bs = Context (zipWith go as bs)
     where go (Binding name1 u1 ::: t1) (Binding name2 u2 ::: t2)
             | name1 == name2, t1 == t2 = Binding name1 (u1 <> u2) ::: t1
             | otherwise                = error "adding inequal contexts"
 
 
-instance (Eq recur, Eq usage, Semiring usage) => Module usage (Context (Binding usage) recur) where
+instance (Eq ty, Eq usage, Semiring usage) => Module usage (Context (Binding usage) ty) where
   u ><< Context as = Context (map (first (fmap (u ><))) as)
 
 
-(|>) :: Context var recur -> Constraint var recur -> Context var recur
+(|>) :: Context var ty -> Constraint var ty -> Context var ty
 (|>) = fmap Context . flip (:) . unContext
 
 infixl 5 |>
 
-emptyContext :: Context var recur
+emptyContext :: Context var ty
 emptyContext = Context []
