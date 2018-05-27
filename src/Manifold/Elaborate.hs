b946f1bce06eed678e8f399c5d567b34a4e5f627
Rob Rix <rob.rix@me.com>
2018-05-27 14:55:17 -0400

    :fire: the Elaborate module.

--- src/Manifold/Elaborate.hs
+++
@@ -1,8 +0,0 @@
-module Manifold.Elaborate where
-
-import Manifold.Constraint
-import Manifold.Expr
-import Manifold.Type
-
-data Elab var = Elab { elabExpr :: Expr (Constraint var (Elab var)) (Elab var), elabType :: Type var }
-  deriving (Eq, Ord)
