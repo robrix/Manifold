018a4124ddcc782f474646fa9f2755d8d37eb492
Rob Rix <rob.rix@me.com>
2018-05-27 16:39:11 -0400

    Skip whitespace after a qname.

--- src/Manifold/Parser.hs
+++ src/Manifold/Parser.hs
@@ -134,7 +134,7 @@ name :: (Monad m, TokenParsing m) => m Name
 name = N <$> identifier
 
 qname :: (Monad m, TokenParsing m) => m Name
-qname = runUnspaced name'
+qname = token (runUnspaced name')
   where name' = makeN <$> typeIdentifier <*> optional (dot *> name')
             <|>     N <$> identifier
         makeN s Nothing  = N s
