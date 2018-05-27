3d2b6fd03a80b2c5dec06a074058cb8b8290a0e4
Rob Rix <rob.rix@me.com>
2018-05-27 13:13:43 -0400

    Rename the Annotated fields.

--- src/Manifold/Name/Annotated.hs
+++ src/Manifold/Name/Annotated.hs
@@ -4,12 +4,12 @@ module Manifold.Name.Annotated where
 import Manifold.Name
 import Manifold.Pretty
 
-data Annotated usage = Annotated { bindingName :: Name, bindingUsage :: usage }
+data Annotated usage = Annotated { annotatedName :: Name, annotatedUsage :: usage }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 instance Named (Annotated usage) where
-  name = name . bindingName
-  setName n b = b { bindingName = n }
+  name = name . annotatedName
+  setName n b = b { annotatedName = n }
 
 instance Pretty usage => Pretty (Annotated usage) where
   prettyPrec d (Annotated name usage) = prettyPrec d name . showChar ' ' . showChar '@' . prettyPrec d usage
