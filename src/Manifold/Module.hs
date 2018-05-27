6dc3cea95812b2125fd040c0efe70a3767bf8166
Rob Rix <rob.rix@me.com>
2018-05-27 16:24:05 -0400

    Pretty-print modules.

--- src/Manifold/Module.hs
+++ src/Manifold/Module.hs
@@ -1,10 +1,17 @@
 module Manifold.Module where
 
+import Data.List
 import Manifold.Declaration
 import Manifold.Name
+import Manifold.Pretty
 
 data Module var def = Module
   { moduleName         :: Name
   , moduleDeclarations :: [Declaration var def]
   }
   deriving (Eq, Ord, Show)
+
+instance (Pretty var, Pretty def) => Pretty (Module var def) where
+  prettyPrec _ (Module name decls)
+    = showString "module" . showChar ' ' . prettys name . showChar ' ' . showString "where" . showChar '\n'
+    . foldr (.) id (intersperse (showChar '\n' . showChar '\n') (map prettys decls))
