8001afb6ac1241e41fc4df9b838b312bbca2fdd5
Rob Rix <rob.rix@me.com>
2018-05-27 14:46:29 -0400

    Stub in a module datatype.

--- src/Manifold/Module.hs
+++ src/Manifold/Module.hs
@@ -1,1 +1,9 @@
 module Manifold.Module where
+
+import Manifold.Declaration
+import Manifold.Name
+
+data Module var def = Module
+  { moduleName         :: Name
+  , moduleDeclarations :: [Declaration var def]
+  }
