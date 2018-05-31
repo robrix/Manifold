128ee799a49fc2818d2bb55d882a46a8e00774c7
Rob Rix <rob.rix@me.com>
2018-05-31 07:26:38 -0400

    Define a Pretty instance for Pattern.

--- src/Manifold/Pattern.hs
+++ src/Manifold/Pattern.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE LambdaCase #-}
 module Manifold.Pattern where
 
 import Manifold.Name
+import Manifold.Pretty
 
 data Pattern
   = Wildcard
   | Variable Name
   | Constructor Name [Pattern]
   deriving (Eq, Ord, Show)
+
+instance Pretty Pattern where
+  prettyPrec d = \case
+    Wildcard -> showChar '_'
+    Variable name -> prettyPrec d name
+    Constructor name patterns -> showParen (d > 10) $ prettyPrec 10 name . foldr (.) id (map (fmap (showChar ' ') . prettyPrec 11) patterns)
