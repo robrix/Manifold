11d80450729bf09772a7d915cd585481d8a8a967
Rob Rix <rob.rix@me.com>
2018-05-30 20:56:43 -0400

    Fix evaluation of data constructors.

--- src/Manifold/Eval.hs
+++ src/Manifold/Eval.hs
@@ -3,6 +3,7 @@ module Manifold.Eval where
 
 import Control.Monad.Effect
 import Control.Monad.Effect.Reader
+import Data.Semilattice.Lower
 import Manifold.Constraint
 import Manifold.Context
 import Manifold.Name
@@ -24,7 +25,7 @@ eval (Term term) = case term of
       env <- contextFilter (((&&) <$> (/= name var) <*> (`elem` freeVariables body)) . constraintName) <$> ask
       pure (value (Abs (name var ::: env) body))
     Pair a b -> fmap value . Pair <$> eval a <*> eval b
-    Data c as -> value . Data c <$> traverse eval as
+    Data c as -> value . Data (name c ::: lowerBound) <$> traverse eval as
   Elim e -> case e of
     ExL pair -> eval pair >>= \ p -> case unValue p of { Pair a _ -> pure a ; _ -> error "exl on non-pair value, should have been caught by typechecker" }
     ExR pair -> eval pair >>= \ p -> case unValue p of { Pair _ b -> pure b ; _ -> error "exr on non-pair value, should have been caught by typechecker" }
