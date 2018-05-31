afaff068d58b0aae0b5ba464339e6a049a995516
Rob Rix <rob.rix@me.com>
2018-05-30 20:01:33 -0400

    Define type constructors.

--- src/Manifold/Type/Intro.hs
+++ src/Manifold/Type/Intro.hs
@@ -5,12 +5,14 @@ import Data.Trifoldable
 import Data.Trifunctor
 import Data.Bifoldable
 import Data.Bifunctor
+import Manifold.Constructor
 import Manifold.Pretty
 
 data IntroT var scope recur
   = UnitT
   | BoolT
   | TypeT
+  | TypeC Constructor [recur]
   | var :-> scope
   | recur :* recur
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
@@ -21,19 +23,21 @@ infixl 7 :*
 
 instance Trifoldable IntroT where
   trifoldMap f g h = \case
-    UnitT    -> mempty
-    BoolT    -> mempty
-    TypeT    -> mempty
-    v :-> b  -> f v <> g b
-    a :* b   -> h a <> h b
+    UnitT     -> mempty
+    BoolT     -> mempty
+    TypeT     -> mempty
+    TypeC _ as -> foldMap h as
+    v :-> b   -> f v <> g b
+    a :* b    -> h a <> h b
 
 instance Trifunctor IntroT where
   trimap f g h = \case
-    UnitT    -> UnitT
-    BoolT    -> BoolT
-    TypeT    -> TypeT
-    v :-> b  -> f v :-> g b
-    a :* b   -> h a :* h b
+    UnitT     -> UnitT
+    BoolT     -> BoolT
+    TypeT     -> TypeT
+    TypeC c as -> TypeC c (map h as)
+    v :-> b   -> f v :-> g b
+    a :* b    -> h a :* h b
 
 instance Bifoldable (IntroT var) where
   bifoldMap = trifoldMap (const mempty)
@@ -46,5 +50,6 @@ instance (Pretty var, Pretty scope, Pretty recur) => Pretty (IntroT var scope re
     UnitT -> showString "Unit"
     BoolT -> showString "Bool"
     TypeT -> showString "Type"
+    TypeC c as -> showParen (d > 10) $ prettyPrec 10 c . foldr (.) id (map (fmap (showChar ' ') . prettyPrec 11) as)
     v :-> b -> showParen (d > 0) $ prettyPrec 1 v . showChar ' ' . showString "->" . showChar ' ' . prettyPrec 0 b
     a :* b -> showParen (d > 7) $ prettyPrec 7 a . showChar ' ' . showChar '*' . showChar ' ' . prettyPrec 8 b
