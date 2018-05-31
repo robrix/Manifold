c5287dfe17b12b560e356442136dccf6757b7561
Rob Rix <rob.rix@me.com>
2018-05-30 20:13:49 -0400

    Redefine declarationSignature as a function.

--- src/Manifold/Declaration.hs
+++ src/Manifold/Declaration.hs
@@ -6,16 +6,16 @@ import Manifold.Pretty
 import Manifold.Term
 import Manifold.Type
 
-data Declaration var def = Declaration
-  { declarationSignature  :: Constraint var (Type var)
-  , declarationDefinition :: def
-  }
+data Declaration var def = Declaration (Constraint var (Type var)) def
   deriving (Eq, Ord, Show)
 
 instance (Pretty var, Pretty def) => Pretty (Declaration var def) where
   prettyPrec _ (Declaration sig def)
     = prettys sig . showChar '\n'
     . prettys (constraintVar sig) . showChar ' ' . showChar '=' . showChar ' ' . prettys def
+
+declarationSignature :: Declaration var def -> Constraint var (Type var)
+declarationSignature (Declaration sig _) = sig
 
 declarationName :: Named var => Declaration var def -> Name
 declarationName = constraintName . declarationSignature
