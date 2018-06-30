5b058e708a43bee01fd18dd9408e938656acfbe8
Rob Rix <rob.rix@me.com>
2018-06-30 14:31:11 -0400

    Define substitutions, values, functions, expressions, and patterns.

--- src/Manifold/Proof/Focusing.hs
+++ src/Manifold/Proof/Focusing.hs
@@ -1,2 +1,30 @@
 module Manifold.Proof.Focusing where
 
+import Manifold.Name
+
+newtype Substitution def = Substitution { unSubstitution :: [Replacement def] }
+
+data Replacement def = def ://: Name
+infix 9 ://:
+
+data Value
+  = Value (Substitution Function) Pattern
+
+data Function
+  = FLam [Pattern -> Expr]
+  | FId
+  | FIdF Name
+
+data Expr
+  = EVal Value
+  | EApp Function Name Value
+  | ECutV Function Value
+  | ECutE Function Expr
+
+data Pattern
+  = Wildcard
+  | PVar Name
+  | PUnit
+  | PPair Pattern Pattern
+  | PInL Pattern
+  | PInR Pattern
