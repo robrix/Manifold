93ad792dfb32e7e6909228ea2f777c79944bee94
Rob Rix <rob.rix@me.com>
2018-07-14 09:11:13 -0400

    Define a convenience to modify the Store.

--- src/Manifold/Store.hs
+++ src/Manifold/Store.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Manifold.Store where
 
 import qualified Data.Map.Monoidal as Monoidal
+import Manifold.Address
+import Manifold.Evaluator
 
 newtype Store address cell = Store { unStore :: Monoidal.Map address cell }
+
+modifyStore :: (Address address cell effects, Member (State (Store address (cell value))) effects)
+            => (Store address (cell value) -> Store address (cell value))
+            -> Evaluator address value effects ()
+modifyStore = modify'
