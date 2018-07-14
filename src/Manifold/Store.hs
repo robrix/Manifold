babc44e863aeb551107458eff375186f55db6d2c
Rob Rix <rob.rix@me.com>
2018-07-14 09:19:01 -0400

    Define a helper to assign into an address.

--- src/Manifold/Store.hs
+++ src/Manifold/Store.hs
@@ -7,6 +7,13 @@ import Manifold.Evaluator
 
 newtype Store address value = Store { unStore :: Monoidal.Map address [value] }
 
+assign :: (Address address effects, Member (State (Store address value)) effects)
+       => address
+       -> value
+       -> Evaluator address value effects ()
+assign address value = modifyStore (Store . Monoidal.insert address [value] . unStore)
+
+
 modifyStore :: Member (State (Store address value)) effects
             => (Store address value -> Store address value)
             -> Evaluator address value effects ()
