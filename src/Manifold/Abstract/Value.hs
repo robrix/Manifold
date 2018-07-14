1d82e36acc60552b6446c16e0372f45541714d74
Rob Rix <rob.rix@me.com>
2018-07-14 14:10:13 -0400

    Add lambda construction/elimination to Value.

--- src/Manifold/Abstract/Value.hs
+++ src/Manifold/Abstract/Value.hs
@@ -1,4 +1,10 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Manifold.Abstract.Value where
 
-class Value value effects where
+import Manifold.Abstract.Evaluator
+import Manifold.Name
+import Manifold.Term
+
+class Value address value effects where
+  lambda :: Name -> Term Name -> Evaluator address value effects value
+  apply :: value -> value -> Evaluator address value effects value
