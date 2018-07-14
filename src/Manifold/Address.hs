07c04d11060cbb5f81f36a80c245b2e41f6fb9e1
Rob Rix <rob.rix@me.com>
2018-07-14 09:02:20 -0400

    Define an Address instance for Monovariant.

--- src/Manifold/Address.hs
+++ src/Manifold/Address.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, FunctionalDependencies #-}
 module Manifold.Address where
 
+import Data.List.NonEmpty (nonEmpty)
 import Data.Monoid (Alt(..), Last(..))
 import Manifold.Evaluator
 import Manifold.Name
+import qualified Data.Set as Set
 
 class Address address cell effects | address -> cell where
   allocCell :: Name -> Evaluator (address value) effects (address value)
@@ -20,3 +22,11 @@ instance Member Fresh effects => Address Precise Last effects where
 
 newtype Monovariant value = Monovariant { unMonovariant :: Name }
   deriving (Eq, Ord, Show)
+
+instance Member NonDet effects => Address Monovariant Set.Set effects where
+  allocCell = pure . Monovariant
+  derefCell _ = traverse (foldMapA pure) . nonEmpty . Set.toList
+
+
+foldMapA :: (Alternative m, Foldable t) => (b -> m a) -> t b -> m a
+foldMapA f = getAlt . foldMap (Alt . f)
