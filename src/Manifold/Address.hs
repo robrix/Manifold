b7aa03aaf8e40fc6fa99404e3e12097eb3e81926
Rob Rix <rob.rix@me.com>
2018-07-14 09:01:33 -0400

    Parameterize Address by the effects.

--- src/Manifold/Address.hs
+++ src/Manifold/Address.hs
@@ -4,9 +4,9 @@ module Manifold.Address where
 import Manifold.Evaluator
 import Manifold.Name
 
-class Address address cell | address -> cell where
-  alloc :: Name -> Evaluator (address value) effects (address value)
-  deref :: address value -> cell value -> Evaluator (address value) effects value
+class Address address cell effects | address -> cell where
+  allocCell :: Name -> Evaluator (address value) effects (address value)
+  derefCell :: address value -> cell value -> Evaluator (address value) effects (Maybe value)
 
 newtype Precise value = Precise { unPrecise :: Int }
   deriving (Eq, Ord, Show)
