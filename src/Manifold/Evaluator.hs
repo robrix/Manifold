1bfddba13c7b25d4819a10db9d51701ce739ff35
Rob Rix <rob.rix@me.com>
2018-07-13 20:18:21 -0400

    Derive some instances for Evaluator.

--- src/Manifold/Evaluator.hs
+++ src/Manifold/Evaluator.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Manifold.Evaluator where
 
 import Control.Monad.Effect
 
 newtype Evaluator address effects a = Evaluator { runEvaluator :: Eff effects a }
+  deriving (Applicative, Functor, Monad)
