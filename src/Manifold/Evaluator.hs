42e05df6de3b0607ce9088a3881f821a45f7fe94
Rob Rix <rob.rix@me.com>
2018-07-14 09:10:27 -0400

    Re-export State.

--- src/Manifold/Evaluator.hs
+++ src/Manifold/Evaluator.hs
@@ -11,6 +11,7 @@ import Control.Monad.Effect.Fresh as X
 import Control.Monad.Effect.NonDet as X
 import Control.Monad.Effect.Reader as X
 import Control.Monad.Effect.Resumable as X
+import Control.Monad.Effect.State as X
 
 newtype Evaluator address value effects a = Evaluator { runEvaluator :: Eff effects a }
   deriving (Applicative, Functor, Monad)
