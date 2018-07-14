80b91f28544b8d6bbb877de8a8714b432ae287fb
Rob Rix <rob.rix@me.com>
2018-07-14 08:34:11 -0400

    Stub in an Address class.

--- src/Manifold/Address.hs
+++ src/Manifold/Address.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Manifold.Address where
+
+import Manifold.Evaluator
+import Manifold.Name
+
+class Address address cell | address -> cell where
+  alloc :: Name -> Evaluator (address value) effects (address value)
+  deref :: address value -> cell value -> Evaluator (address value) effects value
 
 newtype Precise value = Precise { unPrecise :: Int }
   deriving (Eq, Ord, Show)
