f25ce384a6cda3ed4ee470edf11bf81da853274d
Rob Rix <rob.rix@me.com>
2018-07-14 09:26:53 -0400

    Define a lookup function.

--- src/Data/Map/Monoidal.hs
+++ src/Data/Map/Monoidal.hs
@@ -6,3 +6,6 @@ newtype Map k v = Map { unMap :: Map.Map k v }
 
 insert :: (Ord k, Semigroup v) => k -> v -> Map k v -> Map k v
 insert k v = Map . Map.insertWith (<>) k v . unMap
+
+lookup :: Ord k => k -> Map k v -> Maybe v
+lookup k = Map.lookup k . unMap
