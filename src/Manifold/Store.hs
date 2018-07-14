d17b2e0a37a36b3a588b0b4e64681985e42d42d8
Rob Rix <rob.rix@me.com>
2018-07-14 09:39:36 -0400

    assign uses assignCell.

--- src/Manifold/Store.hs
+++ src/Manifold/Store.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE FlexibleContexts, GADTs #-}
 module Manifold.Store where
 
-import qualified Data.Map.Monoidal as Monoidal
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 import qualified Data.Set as Set
 import Manifold.Address
 import Manifold.Evaluator
 
-newtype Store address value = Store { unStore :: Monoidal.Map address (Set.Set value) }
+newtype Store address value = Store { unStore :: Map.Map address (Set.Set value) }
 
 assign :: (Address address effects, Member (State (Store address value)) effects, Ord value)
        => address
        -> value
        -> Evaluator address value effects ()
-assign address value = modifyStore (Store . Monoidal.insert address (Set.singleton value) . unStore)
+assign address value = do
+  Store store <- get
+  cell <- assignCell address value (fromMaybe Set.empty (Map.lookup address store))
+  put (Store (Map.insert address cell store))
 
 deref :: ( Address address effects
          , Member (Resumable (StoreError address value)) effects
@@ -20,13 +24,7 @@ deref :: ( Address address effects
          )
       => address
       -> Evaluator address value effects value
-deref address = gets (Monoidal.lookup address . unStore) >>= maybe (throwResumable (Unallocated address)) pure >>= derefCell address >>= maybe (throwResumable (Uninitialized address)) pure
-
-
-modifyStore :: Member (State (Store address value)) effects
-            => (Store address value -> Store address value)
-            -> Evaluator address value effects ()
-modifyStore = modify'
+deref address = gets (Map.lookup address . unStore) >>= maybe (throwResumable (Unallocated address)) pure >>= derefCell address >>= maybe (throwResumable (Uninitialized address)) pure
 
 
 data StoreError address value result where
