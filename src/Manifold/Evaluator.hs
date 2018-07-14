a9a632a2ad10559f4c04ff2266a8566c4b7e571d
Rob Rix <rob.rix@me.com>
2018-07-14 08:57:27 -0400

    Re-export Resumable.

--- src/Manifold/Evaluator.hs
+++ src/Manifold/Evaluator.hs
@@ -7,6 +7,7 @@ module Manifold.Evaluator
 import Control.Monad.Effect as X
 import Control.Monad.Effect.Fresh as X
 import Control.Monad.Effect.Reader as X
+import Control.Monad.Effect.Resumable as X
 
 newtype Evaluator address effects a = Evaluator { runEvaluator :: Eff effects a }
   deriving (Applicative, Functor, Monad)
