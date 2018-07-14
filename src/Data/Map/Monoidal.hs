178358bbf14ef3c2b0e7ee11d5b2e8ad0498a8c3
Rob Rix <rob.rix@me.com>
2018-07-13 21:22:30 -0400

    Monoidal insertion.

--- src/Data/Map/Monoidal.hs
+++ src/Data/Map/Monoidal.hs
@@ -3,3 +3,6 @@ module Data.Map.Monoidal where
 import qualified Data.Map as Map
 
 newtype Map k v = Map { unMap :: Map.Map k v }
+
+insert :: (Ord k, Semigroup v) => k -> v -> Map k v -> Map k v
+insert k v = Map . Map.insertWith (<>) k v . unMap
