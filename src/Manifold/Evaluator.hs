f31709f9e54ad480766df014fb0b4ae5ace5c8de
Rob Rix <rob.rix@me.com>
2018-07-13 20:21:08 -0400

    Re-export Control.Monad.Effect & Reader.

--- src/Manifold/Evaluator.hs
+++ src/Manifold/Evaluator.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Manifold.Evaluator where
+module Manifold.Evaluator
+( Evaluator(..)
+, module X
+) where
 
-import Control.Monad.Effect
+import Control.Monad.Effect as X
+import Control.Monad.Effect.Reader as X
 
 newtype Evaluator address effects a = Evaluator { runEvaluator :: Eff effects a }
   deriving (Applicative, Functor, Monad)
