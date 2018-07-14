03fc2ca9bccf803a8d5b7a77bb116462d909a001
Rob Rix <rob.rix@me.com>
2018-07-14 08:52:35 -0400

    Re-export Fresh.

--- src/Manifold/Evaluator.hs
+++ src/Manifold/Evaluator.hs
@@ -5,6 +5,7 @@ module Manifold.Evaluator
 ) where
 
 import Control.Monad.Effect as X
+import Control.Monad.Effect.Fresh as X
 import Control.Monad.Effect.Reader as X
 
 newtype Evaluator address effects a = Evaluator { runEvaluator :: Eff effects a }
