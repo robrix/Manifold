5b21e5894072a62f9959098ada849ef9068e314e
Rob Rix <rob.rix@me.com>
2018-07-14 09:05:27 -0400

    Spell out address.

--- src/Manifold/Env.hs
+++ src/Manifold/Env.hs
@@ -7,18 +7,18 @@ import Manifold.Evaluator
 import Manifold.Name
 import Data.Semilattice.Lower
 
-type Env addr = Context (Constraint Name addr)
+type Env address = Context (Constraint Name address)
 
 
-askEnv :: Member (Reader (Env addr)) effects => Evaluator addr effects (Env addr)
+askEnv :: Member (Reader (Env address)) effects => Evaluator address effects (Env address)
 askEnv = ask
 
 
-runEnv :: Effects effects => Evaluator addr (Reader (Env addr) ': effects) a -> Evaluator addr effects a
+runEnv :: Effects effects => Evaluator address (Reader (Env address) ': effects) a -> Evaluator address effects a
 runEnv = runReader lowerBound
 
 
-(.=) :: Member (Reader (Env addr)) effects => Name -> addr -> Evaluator addr effects a -> Evaluator addr effects a
+(.=) :: Member (Reader (Env address)) effects => Name -> address -> Evaluator address effects a -> Evaluator address effects a
 name .= value = local (|> (name ::: value))
 
 infixl 1 .=
