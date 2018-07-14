f5f7fec82596fd0659a7edeff300260bddc0f649
Rob Rix <rob.rix@me.com>
2018-07-14 09:02:26 -0400

    Spacing.

--- src/Manifold/Address.hs
+++ src/Manifold/Address.hs
@@ -11,6 +11,7 @@ class Address address cell effects | address -> cell where
   allocCell :: Name -> Evaluator (address value) effects (address value)
   derefCell :: address value -> cell value -> Evaluator (address value) effects (Maybe value)
 
+
 newtype Precise value = Precise { unPrecise :: Int }
   deriving (Eq, Ord, Show)
 
