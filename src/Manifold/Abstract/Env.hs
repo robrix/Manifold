a5c14f9fec5dcfcee2e88f576c55cd128b1344b5
Rob Rix <rob.rix@me.com>
2018-07-15 13:09:35 -0400

    Define a helper to look up a name in the env.

--- src/Manifold/Abstract/Env.hs
+++ src/Manifold/Abstract/Env.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, FlexibleContexts, TypeOperators #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, GADTs, TypeOperators #-}
 module Manifold.Abstract.Env where
 
 import Manifold.Constraint
@@ -13,6 +13,9 @@ type Env address = Context (Constraint Name address)
 askEnv :: Member (Reader (Env address)) effects => Evaluator address value effects (Env address)
 askEnv = ask
 
+lookupEnv :: (Member (Reader (Env address)) effects, Member (Resumable (EnvError address)) effects) => Name -> Evaluator address value effects address
+lookupEnv name = askEnv >>= maybe (throwResumable (FreeVariable name)) (pure . constraintValue) . contextLookup name
+
 
 runEnv :: Effects effects => Evaluator address value (Reader (Env address) ': effects) a -> Evaluator address value effects a
 runEnv = runReader lowerBound
@@ -22,3 +25,7 @@ runEnv = runReader lowerBound
 name .= value = local (|> (name ::: value))
 
 infixl 1 .=
+
+
+data EnvError address result where
+  FreeVariable :: Name -> EnvError address address
