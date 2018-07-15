5510c1d37b8d8aa714831c2698d23972a62fa47f
Rob Rix <rob.rix@me.com>
2018-07-15 16:57:14 -0400

    We don’t need a kind signature any more.

--- src/Manifold/Abstract/Value.hs
+++ src/Manifold/Abstract/Value.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, KindSignatures, MultiParamTypeClasses #-}
+{-# LANGUAGE GADTs, MultiParamTypeClasses #-}
 module Manifold.Abstract.Value where
 
 import Manifold.Abstract.Evaluator
@@ -13,6 +13,6 @@ class Value address value effects where
   deconstruct :: value -> Evaluator address value effects (Name, [value])
 
 
-data Function value (m :: * -> *) result where
+data Function value m result where
   Lambda :: Name -> m a -> Function value m a
   Apply  :: value -> value -> Function value m value
