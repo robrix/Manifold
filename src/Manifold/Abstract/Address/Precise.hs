55e5394ebc8656f1d474a3c2ed5b160c3071bfaf
Rob Rix <rob.rix@me.com>
2018-07-22 09:31:23 -0400

    Define an Env handler for Precise addresses.

--- src/Manifold/Abstract/Address/Precise.hs
+++ src/Manifold/Abstract/Address/Precise.hs
@@ -3,8 +3,11 @@ module Manifold.Abstract.Address.Precise where
 
 import Data.Monoid (Last(..))
 import qualified Data.Set as Set
+import Manifold.Abstract.Env (Env(..), Environment, askEnv)
 import Manifold.Abstract.Evaluator
 import Manifold.Abstract.Store
+import Manifold.Constraint
+import Manifold.Context
 import Manifold.Pretty
 
 newtype Precise = Precise { unPrecise :: Int }
@@ -12,6 +15,16 @@ newtype Precise = Precise { unPrecise :: Int }
 
 instance Pretty Precise where
   prettyPrec d (Precise i) = prettyParen (d > 10) $ prettyString "Precise" <+> pretty i
+
+
+runEnv :: ( Member (Reader (Environment Precise)) effects
+          , PureEffects effects
+          )
+       => Evaluator Precise value (Env Precise ': effects) a
+       -> Evaluator Precise value effects a
+runEnv = interpret $ \case
+  Lookup name -> askEnv >>= pure . fmap constraintValue . contextLookup name
+  Bind name addr m -> local (|> (name ::: addr)) (runEnv (Evaluator m))
 
 
 runAllocator :: ( Member Fresh effects
